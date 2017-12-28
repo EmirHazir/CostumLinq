5c9381aeaa1244825374e0680c61ef8395f13426
EmirHazir <emircanaziz@gmail.com>
2017-12-28 21:51:01 +0300

    done

---
+++ ConsoleLinq/LinqEM.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLinq
+{
+    #region CustomLinq
+    public static class LinqEM
+    {
+        /// <summary>
+        /// T burada string, inti float, class hepsi olabilir
+        /// <para>
+        /// Bu sizin dizinizdeki eleman türüne göre değişir
+        /// </para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dizi"></param>
+        /// <param name="fncKontrol"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Sorgu<T>(this IEnumerable<T> dizi, Func<T, bool> fncKontrol)
+        {
+            List<T> _filtrelenenElamanlar = new List<T>();
+            foreach (var item in dizi)
+            {
+                if (fncKontrol(item))
+                {
+                    _filtrelenenElamanlar.Add(item);
+                }
+            }
+            return _filtrelenenElamanlar;
+        }
+
+        /// <summary>
+        /// Dizi içinde aranan Kontrol dogrultusunda değer var mı
+        /// <para>
+        /// eleman türünü sorguda kendin bool belirledin
+        /// </para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dizi"></param>
+        /// <param name="funcKontrol"></param>
+        /// <returns></returns>
+        public static bool VarMi<T>(this IEnumerable<T> dizi, Func<T, bool> funcKontrol)
+        {
+            foreach (var item in dizi)
+            {
+                if (funcKontrol(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Diziden gerekli bilgilerin seçilmesini sağlar
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="dizi"></param>
+        /// <param name="fncDegerDondur"></param>
+        /// <returns></returns>
+        public static IEnumerable<TResult> Sec<T, TResult>(this IEnumerable<T> dizi, Func<T, TResult> fncDegerDondur)
+        {
+            List<TResult> _datas = new List<TResult>();
+            foreach (var item in dizi)
+            {
+                _datas.Add(fncDegerDondur(item));
+            }
+            return _datas;
+        }
+    }
+    #endregion
+}
---
+++ ConsoleLinq/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLinq
+{
+
+    class Musteri
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri[] _musteriler = new Musteri[]
+            {
+                new Musteri
+                {
+                    Name = "Emir",
+                    LastName = "Hazır"
+                },
+                new Musteri
+                {
+                    Name = "Can",
+                    LastName = "Hazır"
+                },
+                new Musteri
+                {
+                    Name = "Aziz",
+                    LastName = "Can"
+                },
+                new Musteri
+                {
+                    Name = "Mustafa",
+                    LastName = "Yakup"
+                },
+            };
+
+            var _adlar = _musteriler.Sec(m => new
+            {
+                m.Name
+            });
+            foreach (var item in _adlar)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            var _adlar1 = _musteriler.Sorgu(x => x.Name.Contains("a"));
+            foreach (var item in _adlar1)
+            {
+                Console.WriteLine(item.Name);
+            }
+            string[] _isimler = new string[]
+            {
+                "Emir",
+                "Aziz",
+                "Cem",
+                "Ece"
+            };
+            bool _controlVarMi = _isimler.VarMi((o) => o == "Emir");
+            if (_controlVarMi)
+            {
+                Console.WriteLine(_controlVarMi + " geldi");
+            }
+         
+            Console.ReadKey();
+        }
+    }
+}
